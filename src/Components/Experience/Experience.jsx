fdbc8d0e53e17415b4df51cc383ef2d9bee66acb
Jonas Isaiah Mulingbayan <jonasmulingbayan@gmail.com>
2024-06-11 17:24:52 +0800

    Added Company Images

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,60 +1,9 @@
 import React from 'react';
 import './Experience.css';
 import theme_patern from '../../assets/theme_pattern.svg';
+import Experiences_Data from '../../assets/experiences_data'
 
 const Experience = () => {
-  const experiences = [
-    {
-      company: "In1Go Technologies Inc.",
-      position: "Web Developer",
-      location: "Las Piñas City",
-      date: "September 29, 2023 - Present",
-      description: [
-        "Developed and maintained interactive, responsive websites using HTML, CSS, JavaScript, Bootstrap, WordPress, and Elementor.",
-        "Integrated Google Analytics and Pipedrive CRM to websites.",
-        "Setup UTM Parameters using Google Tag Manager.",
-        "Fixed page indexing in Google Search Console.",
-        "Worked with back-end developers to integrate front-end and back-end functionality, enhancing overall application functionality.",
-        "Conducted thorough testing and debugging, identifying and resolving issues to maintain a high standard of quality.",
-        "Maintained company website by regularly updating content, ensuring accurate information, and improving the user experience."
-      ]
-    },
-    {
-      company: "Spadmond Technology Inc.",
-      position: "Front-End Developer",
-      location: "Makati City",
-      date: "June 26, 2023 - September 2023",
-      description: [
-        "Developed and maintained interactive, responsive web applications using HTML, CSS, JavaScript, Bootstrap, WordPress, and Elementor.",
-        "Fixed Bugs.",
-        "Worked with back-end developers to integrate front-end and back-end functionality, enhancing overall application functionality.",
-        "Conducted thorough testing and debugging, identifying and resolving issues to maintain a high standard of quality."
-      ]
-    },
-    {
-      company: "Phosclay Chemical Manufacturing (On-The-Job Training)",
-      position: "Web Developer / Programmer",
-      location: "Dasmariñas City, Cavite",
-      date: "March 20, 2023 - June 19, 2023",
-      description: [
-        "Developed and maintained interactive, responsive web applications using HTML, CSS, JavaScript, and Bootstrap.",
-        "Developed Inventory Management System using PHP and MySQL for backend.",
-        "Maintained company website by regularly updating content, ensuring accurate information, and improving the user experience, and fixing bugs."
-      ]
-    },
-    {
-      company: "The Parks Pizza (Capstone Project)",
-      position: "Web Developer",
-      location: "Capstone Project",
-      date: "May 2022 - April 2023",
-      description: [
-        "Developed an E-commerce with Inventory Management System and Point-of-Sales.",
-        "Database created using MySQL and developed back-end using PHP.",
-        "Front-end Developer using HTML, CSS, JavaScript, and Bootstrap."
-      ]
-    }
-  ];
-
   return (
     <div id="experience" className='experience sections'>
         <div className="experience-title">
@@ -62,9 +11,12 @@ const Experience = () => {
             <img src={theme_patern} alt="" />
         </div>
       <div className='experience-main-container'>
-      {experiences.map((exp, index) => (
+      {Experiences_Data.map((exp, index) => (
         <div className="experience-card" key={index}>
+        <div className='logo-header'>
+            <img src={exp.company_image} alt={exp.company} />
           <h3 className="experience-company">{exp.company}</h3>
+          </div>
           <p className="experience-position">{exp.position}</p>
           <p className="experience-details">{exp.location} | {exp.date}</p>
           <ul className="experience-description">
---
+++ src/assets/experiences_data.js
@@ -0,0 +1,63 @@
+import company1 from '../assets/company/in1gotechnology-logo.png'
+import company2 from '../assets/company/spadmond-logo.png'
+import company3 from '../assets/company/pcm-logo.png'
+import company4 from '../assets/company/tpp-logo.png'
+
+const experiences_data = [
+    {
+      company: "In1Go Technologies Inc.",
+      position: "Web Developer",
+      location: "Las Piñas City",
+      company_image: company1,
+      date: "September 29, 2023 - Present",
+      description: [
+        "Developed and maintained interactive, responsive websites using HTML, CSS, JavaScript, Bootstrap, WordPress, and Elementor.",
+        "Integrated Google Analytics and Pipedrive CRM to websites.",
+        "Setup UTM Parameters using Google Tag Manager.",
+        "Fixed page indexing in Google Search Console.",
+        "Worked with back-end developers to integrate front-end and back-end functionality, enhancing overall application functionality.",
+        "Conducted thorough testing and debugging, identifying and resolving issues to maintain a high standard of quality.",
+        "Maintained company website by regularly updating content, ensuring accurate information, and improving the user experience."
+      ]
+    },
+    {
+      company: "Spadmond Technology Inc.",
+      position: "Front-End Developer",
+      location: "Makati City",
+      company_image: company2,
+      date: "June 26, 2023 - September 2023",
+      description: [
+        "Developed and maintained interactive, responsive web applications using HTML, CSS, JavaScript, Bootstrap, WordPress, and Elementor.",
+        "Fixed Bugs.",
+        "Worked with back-end developers to integrate front-end and back-end functionality, enhancing overall application functionality.",
+        "Conducted thorough testing and debugging, identifying and resolving issues to maintain a high standard of quality."
+      ]
+    },
+    {
+      company: "Phosclay Chemical Manufacturing (On-The-Job Training)",
+      position: "Web Developer / Programmer",
+      location: "Dasmariñas City, Cavite",
+      company_image: company3,
+      date: "March 20, 2023 - June 19, 2023",
+      description: [
+        "Developed and maintained interactive, responsive web applications using HTML, CSS, JavaScript, and Bootstrap.",
+        "Developed Inventory Management System using PHP and MySQL for backend.",
+        "Maintained company website by regularly updating content, ensuring accurate information, and improving the user experience, and fixing bugs."
+      ]
+    },
+    {
+      company: "The Parks Pizza (Capstone Project)",
+      position: "Web Developer",
+      location: "Capstone Project",
+      company_image: company4,
+      date: "May 2022 - April 2023",
+      description: [
+        "Developed an E-commerce with Inventory Management System and Point-of-Sales.",
+        "Database created using MySQL and developed back-end using PHP.",
+        "Front-end Developer using HTML, CSS, JavaScript, and Bootstrap."
+      ]
+    }
+  ]
+
+
+  export default experiences_data;
