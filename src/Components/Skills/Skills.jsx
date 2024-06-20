f1dcad0c57b1089af0c4f76514d7877e43a2c11d
Jonas Isaiah Mulingbayan <jonasmulingbayan@gmail.com>
2024-06-20 17:44:44 +0800

    Update Skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = ({ theme }) => {
         <p>Proficient in front-end web development technologies such as HTML5, CSS, and JavaScript, with a strong grasp of responsive design principles. Skilled in utilizing both Bootstrap and Tailwind CSS frameworks for building stylish and user-friendly interfaces. Familiarity with WordPress and Elementor for rapid website development and customization.</p>
         <p>Experienced in back-end development using PHP, with the ability to create robust and scalable web applications. Proficient in database management with MySQL, ensuring efficient data storage and retrieval.</p>
         <p>Knowledgeable in modern development tools like Vite for fast-paced development workflows. Competent in version control using Git for effective collaboration and project management.</p>
-        <p>Currently exploring ReactJS for building dynamic and interactive web applications.</p>
+        <p>Currently exploring ReactJS and Laravel for building dynamic and interactive web applications.</p>
       </div>
       <div className="skills-grid">
         {skills_data.map(skill => (
